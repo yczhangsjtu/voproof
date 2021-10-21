f0b2035e605f9dcab141255990cf7d4a83705f9e
Yuncong Zhang <yczhangsjtu@gmail.com>
2021-10-21 19:46:37 +0800

    Update.

--- src/snarks/mod.rs
+++ src/snarks/mod.rs
@@ -1,33 +1,21 @@
-mod voproof_r1cs;
 mod voproof_hpr;
 mod voproof_pov;
+mod voproof_r1cs;
 // mod template;
 
-use ark_ec::{
-    PairingEngine, AffineCurve, ProjectiveCurve
-};
-use ark_ff::{
-    Field,
-    PrimeField,
-    FftField, FpParameters,
-    fields::batch_inversion
-};
-use ark_poly_commit::{UVPolynomial};
-#[macro_use]
+use ark_ec::{AffineCurve, PairingEngine, ProjectiveCurve};
+use ark_ff::{fields::batch_inversion, FftField, Field, FpParameters, PrimeField};
+use ark_poly_commit::UVPolynomial;
 use ark_ff::to_bytes;
-use ark_poly::univariate::DensePolynomial as DensePoly;
-use ark_std::{test_rng, Zero, One, vec::Vec, ops::{Mul, Add, Sub}};
-use crate::kzg::{
-    UniversalParams, Powers, VerifierKey,
-    Proof as KZGProof
+use crate::cs::{
+  r1cs::{R1CSInstance, R1CSSize, R1CSWitness, R1CS},
+  CSSize, ConstraintSystem, Instance, Witness,
 };
 use crate::error::Error;
-use crate::cs::{
-    CSSize, ConstraintSystem, Instance, Witness,
-    r1cs::{R1CS, R1CSSize, R1CSInstance, R1CSWitness}
-};
+use crate::kzg::{Proof as KZGProof, UniversalParams, VerifierKey};
 use crate::tools::*;
-#[macro_use]
+use ark_poly::univariate::DensePolynomial as DensePoly;
+use ark_std::{ops::Mul, test_rng, vec::Vec, One, Zero};
 use crate::*;
 use crate::kzg::*;
 
@@ -35,29 +23,31 @@ pub trait SNARKProverKey<E: PairingEngine> {}
 pub trait SNARKVerifierKey<E: PairingEngine> {}
 pub trait SNARKProof<E: PairingEngine> {}
 
-pub fn vector_to_commitment<E: PairingEngine>(powers: &Vec<E::G1Affine>, vec: &Vec<E::Fr>)
-        -> Result<Commitment<E>, Error> {
-    KZG10::<E, DensePoly<E::Fr>>::commit_with_coefficients(powers, vec)
+pub fn vector_to_commitment<E: PairingEngine>(
+  powers: &Vec<E::G1Affine>,
+  vec: &Vec<E::Fr>,
+) -> Result<Commitment<E>, Error> {
+  KZG10::<E, DensePoly<E::Fr>>::commit_with_coefficients(powers, vec)
 }
 
-pub fn scalar_to_commitment<E: PairingEngine>(g: &E::G1Affine, c: &E::Fr)
-        -> Result<Commitment<E>, Error> {
-    KZG10::<E, DensePoly<E::Fr>>::commit_single(g, c)
+pub fn scalar_to_commitment<E: PairingEngine>(
+  g: &E::G1Affine,
+  c: &E::Fr,
+) -> Result<Commitment<E>, Error> {
+  KZG10::<E, DensePoly<E::Fr>>::commit_single(g, c)
 }
 
 pub trait SNARK<E: PairingEngine> {
-    type Size: CSSize;
-    type CS: ConstraintSystem<E::Fr, Self::Size>;
-    type PK: SNARKProverKey<E>;
-    type VK: SNARKVerifierKey<E>;
-    type Ins: Instance<E::Fr>;
-    type Wit: Witness<E::Fr>;
-    type Pf: SNARKProof<E>;
+  type Size: CSSize;
+  type CS: ConstraintSystem<E::Fr, Self::Size>;
+  type PK: SNARKProverKey<E>;
+  type VK: SNARKVerifierKey<E>;
+  type Ins: Instance<E::Fr>;
+  type Wit: Witness<E::Fr>;
+  type Pf: SNARKProof<E>;
 
-    fn setup(size: usize) -> Result<UniversalParams<E>, Error>;
-    fn index(pp: &UniversalParams<E>, cs: &Self::CS)
-        -> Result<(Self::PK, Self::VK), Error>;
-    fn prove(pk: &Self::PK, x: &Self::Ins, w: &Self::Wit) -> Result<Self::Pf, Error>;
-    fn verify(vk: &Self::VK, x: &Self::Ins, proof: &Self::Pf) -> Result<(), Error>;
+  fn setup(size: usize) -> Result<UniversalParams<E>, Error>;
+  fn index(pp: &UniversalParams<E>, cs: &Self::CS) -> Result<(Self::PK, Self::VK), Error>;
+  fn prove(pk: &Self::PK, x: &Self::Ins, w: &Self::Wit) -> Result<Self::Pf, Error>;
+  fn verify(vk: &Self::VK, x: &Self::Ins, proof: &Self::Pf) -> Result<(), Error>;
 }
-
